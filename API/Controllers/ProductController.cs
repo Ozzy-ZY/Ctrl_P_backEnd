6f456fd623e5b015b55a90e6e7acb6767886223f
Ozzy-ZY <Zyadhimself1@gmail.com>
2024-10-13 22:53:02 +0300

    Added some Structure to the Project and Started Implementing the Repository and UnitOfWork Patterns Started With Products

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddProduct([FromBody] ProductDTO productDTO)
+        {
+        }
+    }
+}
---
+++ DataAccess/DataAccess/Repositories/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DataAccess.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
