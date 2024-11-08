221f63ca3a5c9daffb161bcbcab0e31378286789
Ozzy-ZY <Zyadhimself1@gmail.com>
2024-11-08 13:26:02 +0200

    changed the way the connection string is passed to the DbFactory

--- DataAccess/DataAccess/AppDbContextFactory.cs
+++ DataAccess/DataAccess/AppDbContextFactory.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace Infrastructure.DataAccess;
 public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
+    private readonly IConfiguration _configuration;
+
+    public AppDbContextFactory(IConfiguration config)
+    {
+        _configuration = config;
+    }
     public AppDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseSqlServer("Data Source=OZZY;Initial Catalog=Ctrl_P;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
 
         return new AppDbContext(optionsBuilder.Options);
     }
