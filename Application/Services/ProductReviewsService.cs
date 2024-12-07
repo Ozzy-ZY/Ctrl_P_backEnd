dc2ae831703c4c4db90b6b49e48f81e4e47f40f2
Seif357 <seifomar3579@gmail.com>
2024-12-07 19:57:10 +0200

    Made authorization for deleting and updating the reviews

--- Application/Services/ProductReviewsService.cs
+++ Application/Services/ProductReviewsService.cs
@@ -1,6 +1,7 @@
 ﻿using Application.DTOs;
 using Application.DTOs.Mappers;
 using Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,12 +24,35 @@ namespace Application.Services
         }
         public async Task<int> UpdateProductReviewAsync(ProductReviewsDto productReviewDto)
         {
-            await _unitOfWork.ProductReviews.UpdateAsync(productReviewDto.ToProductReview());
+            // Fetch the existing review
+            var existingReview = await _unitOfWork.ProductReviews.GetAsync(p => p.Id == productReviewDto.Id);
+
+
+            // Check if the reviewer IDs match
+            if (existingReview.ReviewerId != productReviewDto.ReviewerId)
+            {
+                throw new UnauthorizedAccessException("You do not have permission to update this review.");
+            }
+            existingReview.Review = productReviewDto.Review;
+            existingReview.Rating = productReviewDto.Rating;
+            existingReview.ReviewDate = DateTime.Now;
+            existingReview.Name = productReviewDto.ReviewerName;
+            existingReview.Email = productReviewDto.ReviewerEmail;
             return await _unitOfWork.CommitAsync();
         }
+
         public async Task<int> DeleteProductReviewAsync(ProductReviewsDto productReviewDto)
         {
-            await _unitOfWork.ProductReviews.DeleteAsync(productReviewDto.ToProductReview());
+            // Fetch the existing review from the database
+            var existingReview = await _unitOfWork.ProductReviews.GetAsync(p=>p.Id==productReviewDto.Id);
+
+            // Check if the reviewer IDs match
+            if (existingReview.ReviewerId != productReviewDto.ReviewerId)
+            {
+                throw new UnauthorizedAccessException("You do not have permission to delete this review.");
+            }
+
+            await _unitOfWork.ProductReviews.DeleteAsync(existingReview);
             return await _unitOfWork.CommitAsync();
         }
     }
