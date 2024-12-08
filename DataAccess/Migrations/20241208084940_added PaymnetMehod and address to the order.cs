106549cf6c96585f9e16ccb7e0f57182df336cc6
Ozzy-ZY <Zyadhimself1@gmail.com>
2024-12-08 10:51:52 +0200

    added the new properties to the address and order and their relationships and added a StaticData.cs file to hold our static Data so that we don't make unnesessary mistakes

---
+++ DataAccess/Migrations/20241208084940_added PaymnetMehod and address to the order.cs
@@ -0,0 +1,214 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedPaymnetMehodandaddresstotheorder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuildingNumber",
+                table: "Addresses");
+
+            migrationBuilder.RenameColumn(
+                name: "StreetName",
+                table: "Addresses",
+                newName: "ZipCode");
+
+            migrationBuilder.RenameColumn(
+                name: "AddressString",
+                table: "Addresses",
+                newName: "StreetAddress");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AddressId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrderStatus",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentMethod",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressText",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingAddress",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyName",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { 1, null, "Admin", "ADMIN" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "ef26a487-78fc-46a2-8cc7-679c9baba6d8");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AddressId",
+                table: "Orders",
+                column: "AddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Addresses_AddressId",
+                table: "Orders",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Addresses_AddressId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_AddressId",
+                table: "Orders");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "OrderStatus",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "AddressText",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "BillingAddress",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyName",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Addresses");
+
+            migrationBuilder.RenameColumn(
+                name: "ZipCode",
+                table: "Addresses",
+                newName: "StreetName");
+
+            migrationBuilder.RenameColumn(
+                name: "StreetAddress",
+                table: "Addresses",
+                newName: "AddressString");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BuildingNumber",
+                table: "Addresses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "060ba078-6ee1-4978-90b7-e99feafb851f");
+        }
+    }
+}
